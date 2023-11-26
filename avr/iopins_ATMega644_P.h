31bc24e1c4fdabc2393b227e7a92fb490cc4b8f3
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-11-26 21:42:49 +0100

    Add pin mode

--- avr/iopins_ATMega644_P.h
+++ avr/iopins_ATMega644_P.h
@@ -3,9 +3,15 @@
 
 #include <avr/io.h>
 
-#define IO_PIN_COUNT_TOTAL		32
-#define IO_PIN_COUNT_DIGITAL	24
-#define IO_PIN_COUNT_ANALOG		8
+#ifndef __AVR_ATmega644P__
+#ifndef __AVR_ATmega644__
+	#pragma warning "This header is only for use with ATMega 644. Pinout information may be inaccurate."
+#endif
+#endif
+
+#define IO_PIN_COUNT_TOTAL		32	// Total pins count
+#define IO_PIN_COUNT_DIGITAL	24	// Digital pins count
+#define IO_PIN_COUNT_ANALOG		8	// Analog pins count
 
 #define __PORT	0
 #define __DDR	1
@@ -18,9 +24,7 @@
 	array:
 		0:	PORT
 		1:	DDR
-		2:	PIN
-
-*/
+		2:	PIN						*/
 typedef volatile uint8_t* pin[3];
 
 #define PIN00	{ &PORTB,	&DDRB,	&PB0 }
@@ -59,7 +63,6 @@ typedef volatile uint8_t* pin[3];
 #define PIN30	{ &PORTA,	&DDRA,	&PA6 }
 #define PIN31	{ &PORTA,	&DDRA,	&PA7 }
 
-
 #define ANALOG0	PIN24
 #define ANALOG1	PIN25
 #define ANALOG2	PIN26
@@ -68,28 +71,38 @@ typedef volatile uint8_t* pin[3];
 #define ANALOG5	PIN29
 #define ANALOG6	PIN30
 #define ANALOG7	PIN31
-
+ 
 #define GET_PIN(PIN)			( PIN[__PIN ] )
 #define GET_DDR(PIN)			( PIN[__DDR ] )
 #define GET_PORT(PIN)			( PIN[__PORT] )
 
-#define SET_BIT(PORT, BIT	)	( *PORT |=  (1 << BIT)	)
-#define UNSET_BIT(PORT, BIT	)	( *PORT &= ~(1 << BIT)	)
-#define GET_BIT(PORT, BIT	)	( (PORT >> BIT) & 0x01	)
+#ifndef _BITS_H_
+#define _BITS_H_
+	#define SET_BIT(   PORT, BIT)	( *PORT |=	 (1 << BIT)	)
+	#define UNSET_BIT( PORT, BIT)	( *PORT &=	~(1 << BIT)	)
+	#define TOGGLE_BIT(PORT, BIT)	( *PORT ^=   (1 << BIT) )
+	#define GET_BIT(   PORT, BIT)	( (*PORT >> BIT) & 0x01	)
+#endif
 
-#define DIGITAL_WRITE_LOW(PIN)	( UNSET_BIT( GET_PORT(PIN), PIN) )
-#define DIGITAL_WRITE_HIGH(PIN)	( SET_BIT(	 GET_PORT(PIN), PIN) )
+#define DIGITAL_WRITE_LOW(PIN)	( UNSET_BIT( GET_PORT(PIN), GET_PIN(PIN)) )
+#define DIGITAL_WRITE_HIGH(PIN)	( SET_BIT(	 GET_PORT(PIN), GET_PIN(PIN)) )
 
-#define DIGITAL_READ(PIN)		( GET_BIT (GET_PORT(PIN), PIN))
-
-#define DIGITAL_WRITE(PIN, VAL)	( VAL ?							\
-								( DIGITAL_WRITE_HIGH(PIN)) :	\
-								( DIGITAL_WRITE_LOW( PIN))
+#define DIGITAL_READ(PIN)		( GET_BIT (	 GET_PORT(PIN),	GET_PIN(PIN)) )
+#define DIGITAL_WRITE(PIN, VAL)	( VAL ?									\
+								( DIGITAL_WRITE_HIGH(GET_PIN(PIN) )) :	\
+								( DIGITAL_WRITE_LOW( GET_PIN(PIN) ))
+#define DIGITAL_SWITCH(PIN)		( TOGGLE_BIT(GET_PORT(PIN), GET_PIN(PIN)) )
 
 #define LOW		0
 #define HIGH	1
 
-#define OFF		0
-#define ON		1
+#define DIGITAL_SET_MODE_OUTPUT(PIN)	( UNSET_BIT(GET_DDR(PIN), PIN) )
+#define DIGITAL_SET_MODE_INPUT( PIN)	( SET_BIT(	GET_DDR(PIN), PIN) )
+#define DIGITAL_SET_MODE(PIN, MODE)		( MODE ?							\
+										( DIGITAL_SET_MODE_OUTPUT(PIN)) :	\
+										( DIGITAL_SET_MODE_INPUT( PIN)) )
+
+#define IO_MODE_OUT	1
+#define IO_MODE_IN	0
 
 #endif
